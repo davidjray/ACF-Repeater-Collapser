fe4a1c11e0479251c3729d8a205d30bb85185416
David Ray <david@raycreative.com>
2013-07-18 18:10:38 -0700

    Added a new feature that allows the sub fields to be separately collapsed.
    Also added a new feature that "Hides" the sub fields. This way repeaters with 20 plus rows can be managed better as well.

--- js/acf_repeater_collapser_admin.js
+++ js/acf_repeater_collapser_admin.js
@@ -14,20 +14,57 @@ jQuery(document).ready(function($) {
 	    }
 	}
 
+	// toggle the class that hides the repeater sub fields
+	// toggle appropriate button text
+	function acf_repeater_hide_toggle( event ) {
+	    // Toggle Fields
+	    $(this).parent().toggleClass('hidden-repeater');
+	    
+	    // Nice Button Text
+	    if( $(this).attr('value') == 'Hide Fields' ) {
+	    	$(this).attr('value', 'Show Fields');
+	    } else {
+	    	$(this).attr('value', 'Hide Fields');
+	    }
+	}
+
 	// HTML to put above each repeater instance
 	$collapseButton = '<input class="button field-repeater-toggle" type="button" value="Collapse Fields" />';
 
+	// HTML to put above each repeater instance for HIDE button
+	$hideButton = '<input class="button field-repeater-hide-toggle" type="button" value="Hide Fields" />';
+
 	// find each repeater instance, add the button if the field uses the row layout
-	$('.field_type-repeater, .field_type-flexible_content').each( function() {
+	$('.field_type-repeater .repeater, .field_type-flexible_content .repeater').each( function() {
 		if( $( '.acf-input-table', $(this) ).hasClass('row_layout') ) {
 			$(this).prepend( $collapseButton );
 		}
 	});
+
+	// find each repeater instance, add the button if the field uses the row layout
+	$('.field_type-repeater .repeater, .field_type-flexible_content .repeater').each( function() {
+		//if( $( '.acf-input-table', $(this) ).hasClass('row_layout') ) {
+			$(this).prepend( $hideButton );
+
+			// Makes the fields hidden by default.
+			$(this).addClass('hidden-repeater');
+			$( '.field-repeater-hide-toggle' ).attr('value', 'Show Fields' )
+		//}
+	});
+
 
 	// bind the click event to the toggle function
 	$( '.field-repeater-toggle' ).on(
 		'click',
 		acf_repeater_toggle
 	)
+
+	// bind the click event to the HIDE toggle function
+	$( '.field-repeater-hide-toggle' ).on(
+		'click',
+		acf_repeater_hide_toggle
+	)
+
+
 
 });
